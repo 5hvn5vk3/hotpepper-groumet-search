e3578a988bdac662454d7517345599fd52ec8a91
5hvn5vk3 <kusami0907@yahoo.co.jp>
2026-02-24 18:34:47 +0900

    fix(UI): 残っていた「約」の削除

--- frontend/src/components/RestaurantDetail.tsx
+++ frontend/src/components/RestaurantDetail.tsx
@@ -97,7 +97,7 @@ export const RestaurantDetail: React.FC<RestaurantDetailProps> = ({
                   現在地からの距離
                 </h4>
                 <p className="text-green-600 font-medium">
-                  📍 約
+                  📍
                   {formatDistance(
                     calculateDistance(
                       userLat,
